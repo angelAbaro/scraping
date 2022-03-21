ffe309987efcc41c1819104e38a35e452d09655d
Angel Abaro <angelm.abaro@gmail.com>
2022-03-21 11:55:47 -0500

    nuevas modificaciones y archivos

--- scraping curso/nivel1_wikipedia_angel.py
+++ scraping curso/nivel1_wikipedia_angel.py
@@ -1,5 +1,6 @@
 # reques es para hacer el requerimiento al servidor
 import requests
+from lxml import html # pip install lxml
 # User Agents // esto para que no nos identifique coo robot
 # Es una cadena de texto con la cual se puede identificar el navegador y el sistema operativo del cliente. Por defecto es: ROBOT
 
@@ -15,5 +16,24 @@ url = "https://www.wikipedia.org/"
 #aqui tambien le pasamos encabezados a travez del parametro headers
 respuesta = requests.get(url, headers=encabezados)
 
-print(respuesta.text)
+# PARSEO DEL ARBOL HTML QUE RECIBO COMO RESPUESTA CON LXML
+parser = html.fromstring(respuesta.text)
+
+# EXTRACCION DE IDIOMA INGLES
+ingles = parser.get_element_by_id("js-link-box-en")
+print (ingles.text_content())
+
+# EXTRACCION SOLO DEL TEXTO QUE DICE INGLES
+ingles = parser.xpath("//a[@id='js-link-box-en']/strong/text()")
+print(ingles[0])
+
+# EXTRACCION DE TODOS LOS IDIOMAS POR CLASE
+idiomas = parser.find_class('central-featured-lang')
+for idioma in idiomas:
+  print(idioma.text_content())
+
+# EXTRACCION DE TODOS LOS IDIOMAS POR XPATH
+idiomas = parser.xpath("//div[contains(@class,'central-featured-lang')]//strong/text()")
+for idioma in idiomas:
+  print(idioma)
 
